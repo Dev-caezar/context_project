a069f1be5d7f6dda18dc7c2492e0709126128169
Caezar-coder <okochristian6@gmail.com>
2025-03-21 12:00:48 +0100

    made some update in the dashboard

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdOutlineWavingHand } from "react-icons/md";
+import { FaSearchDollar } from "react-icons/fa";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { IoIosArrowDown } from "react-icons/io";
 import "./header.css"
 
 const Header = () => {
+    const [drop, setDrop] = useState(false)
+
+    const handleDrop =()=>{
+        setDrop((prev)=> !prev)
+    }
   return (
     <div className='header_body'>
-     
+     <div className="header_wrapper">
+        <div className="left">
+            <div className="profile_img">
+                <img src="/src/assets/public/my pics.jpg" alt="" />
+            </div>
+            <div className="greetings">
+                <h4>Greetings!</h4>
+                <MdOutlineWavingHand className='wave'/>
+            </div>
+        </div>
+        <div className="right">
+            <div className="search">
+                <FaSearchDollar />
+                <input type="text" className='search_transaction'placeholder='Search transactions, cards, etc'/>
+            </div>
+            <div className="acct_type" onClick={handleDrop}>
+                <MdOutlineAccountCircle className='acct_icon'/>
+                <p>Accounts</p>
+                <IoIosArrowDown className='acct_icon' style={{ transform: `rotate(${drop ? 180 : 0}deg)` }} />
+
+                {
+                    drop? <div className="">
+                        <p>Savings</p>
+                        <p>Current</p>
+                        <p>Dollar</p>
+                    </div>:
+                    null
+                }
+            </div>
+        </div>
+     </div>
     </div>
   )
 }
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,13 +1,131 @@
-import React from 'react'
-import "./dashboard.css"
+import React, { useState } from 'react';
+import "./dashboard.css";
+import {Table, Tag } from 'antd';
+import { CiMenuKebab } from "react-icons/ci";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+
+const columns = [
+  {
+    title: 'Sender',
+    dataIndex: 'sender',
+    key: 'sender',
+  },
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+    key: 'amount',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+    render: (status) => (
+      <Tag color={status === "Success" ? "green" : "red"}>{status}</Tag>
+    ),
+  },
+  {
+    title: 'Date',
+    dataIndex: 'date',
+    key: 'date',
+  },
+];
+
+const data = [
+  { key: 1, sender: 'John Brown', amount: 32, status: 'Success', date: "10 Feb 2024" },
+  { key: 2, sender: 'Jim Green', amount: 42, status: 'Pending', date: "10 Feb 2024" },
+  { key: 3, sender: 'Not Expandable', amount: 29, status: 'Success', date: "10 Feb 2024" },
+  { key: 4, sender: 'Not Expandable', amount: 29, status: 'Pending', date: "10 Feb 2024" },
+  { key: 5, sender: 'Not Expandable', amount: 29, status: 'Pending', date: "10 Feb 2024" },
+  { key: 6, sender: 'Not Expandable', amount: 29, status: 'Success', date: "10 Feb 2024" },
+  { key: 7, sender: 'Not Expandable', amount: 29, status: 'Success', date: "10 Feb 2024" },
+  { key: 8, sender: 'Not Expandable', amount: 29, status: 'Success', date: "10 Feb 2024" },
+  { key: 9, sender: 'Not Expandable', amount: 29, status: 'Success', date: "10 Feb 2024" },
+  { key: 10  , sender: 'Not Expandable', amount: 29, status: 'Success', date: "10 Feb 2024" },
+];
+
 const Dashboard = () => {
+  const [show,setShow]= useState(false)
+
+  const handleShowAmount =()=>{
+    setShow((prev)=> !prev)
+  }
+  const [back,setBack]= useState(false)
+
+  const handleDisplayAmount =()=>{
+    setBack((prev)=> !prev)
+  }
   return (
     <div className='dashboardBody'>
-      <div className="acct_show">
-        <div className="card_one"></div>
+      <div className="acct_details">
+        <div className="details_header">
+          <h2>Cards</h2>
+          <h4>See all</h4>
+        </div>
+        <div className="card_container">
+          <div className="card_front">
+            <div className="card_header">
+              <CiMenuKebab />
+            </div>
+            <div className="card_amount">
+                {
+                  show? 
+                  <h4>$5,000.00</h4>:
+                  <h4>****</h4>
+                }
+              </div>
+              <div className="card_date">
+                <div className="date">
+                <p>20/05/2025</p>
+                {
+                  show?
+                  <FaRegEyeSlash  onClick={handleShowAmount}/> :
+                  <FaRegEye onClick={handleShowAmount}/>
+                }
+                </div>
+                <div className="card_type">
+                  <p>VISA</p>
+                </div>
+              </div>
+          </div>
+          <div className="card_back">
+          <div className="card_header">
+              <CiMenuKebab />
+            </div>
+            <div className="card_amount">
+                {
+                  back? 
+                  <h4>#21,420.00</h4>:
+                  <h4>****</h4>
+                }
+              </div>
+              <div className="card_date">
+                <div className="date">
+                <p>10/12/2025</p>
+                {
+                  back?
+                  <FaRegEyeSlash  onClick={handleDisplayAmount}/> :
+                  <FaRegEye onClick={handleDisplayAmount}/>
+                }
+                </div>
+                <div className="card_type">
+                  <p>VERVE</p>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div className="acct_table">
+          <h4>Recent transactions</h4>
+          <Table 
+            columns={columns} 
+            dataSource={data} 
+            pagination={{ pageSize: window.innerWidth < 400 ? 5 : 4}} 
+            scroll={{ x: true }} 
+          />
+        </div>
       </div>
+      <div className="chart"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
